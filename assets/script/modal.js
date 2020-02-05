8dd8cdcf8c398fdbb98038214228caa5e63ec60c
UnderMorph <d.dimension@yandex.ru>
2020-02-06 01:43:40 +0400

    Оживил модалку с картой, исправил форму входа на внутренних страницах

--- assets/script/modal.js
+++ assets/script/modal.js
@@ -6,6 +6,9 @@ var form = popup.querySelector("form");
 var password = popup.querySelector("[name=password]");
 var isStorageSupport = true;
 var storage = ""
+var mapLink = document.querySelector(".js-button-map");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = document.querySelector(".modal-map-close");
 
 try {
     storage = localStorage.getItem("item");
@@ -53,5 +56,24 @@ window.addEventListener("keydown", function (evt) {
             popup.classList.remove("modal-error");
         }
     }
-})
+});
+
+mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+    evt.preventDefault();
+    if (evt.keyCode === 27) {
+        if (mapPopup.classList.contains("modal-show")) {
+            mapPopup.classList.remove("modal-show");
+        }
+    }
+});
 
